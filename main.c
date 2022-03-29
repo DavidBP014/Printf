339ac1a630904d2511d823cf4ecf6295c11ad390
DavidBP014 <juanbp014@gmail.com>
2022-03-28 20:16:14 -0500

    hh

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 /**
  * main - Entry point
